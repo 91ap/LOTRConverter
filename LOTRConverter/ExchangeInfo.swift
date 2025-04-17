6512f18868951e151829637a294229273a30ad1e
Andy Patterson <andyp.dev@proton.me>
2025-04-17 02:06:14 -0500

    [Task] Added Exchange info view via SwiftUI, Tested dark mode, reviewed and modified views

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by apat on 4/17/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
