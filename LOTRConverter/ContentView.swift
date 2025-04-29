02d0c8db8fdaadb5d782cf658296cc3c3799f68e
Andy Patterson <andyp.dev@proton.me>
2025-04-29 05:16:15 -0500

    [Task] Currency Model: updated argument label in the convert function. ContentView: updated textField, added modifiers

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -14,7 +14,10 @@ struct ContentView: View {
    @State var leftAmount = ""
    @State var rightAmount = ""
     
-    @State var leftCurrency = Currency.silverPiece
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
+   @State var leftCurrency = Currency.silverPiece
    @State var rightCurrency: Currency = .goldPiece
     
     var body: some View {
@@ -59,6 +62,13 @@ struct ContentView: View {
                         // Text Field
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .focused($leftTyping)
+                            .onChange(of: leftAmount){
+                                if leftTyping {
+                                    rightAmount = leftCurrency
+                                        .convert(leftAmount, to: rightCurrency)
+                                }
+                            }
                     }
                     // Equal sign
                     Image(systemName: "equal")
@@ -90,6 +100,13 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .focused($rightTyping)
+                            .onChange(of: rightAmount) {
+                                if rightTyping {
+                                    leftAmount = rightCurrency
+                                        .convert(rightAmount, to: leftCurrency)
+                                }
+                            }
                     }
                 }
                 .padding()
